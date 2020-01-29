d44f70df14ad69112e4c82c2a761108122c09cc3
iyan <iyan.cupliz@gmail.com>
2020-01-29 12:44:46 +0700

    pure html

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Admin Webcomponents
+# Admin HTML
--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-const route = (name = 'users', el) => {
-  const pages = ['users', 'posts', 'comments']
-  for (const page of pages) {
-    const elPage = document.querySelector(`page-${page}`)
-    if (elPage) {
-      if (name == page) {
-        elPage.removeAttribute('hidden')
-        // if (el) el.parentElement.className += 'active'
-      } else {
-        if (!elPage.hasAttribute('hidden')) {
-          elPage.setAttribute('hidden', '')
-          // if (el) el.parentElement.classList.remove("active")
-        }
-      }
-    }
+localStorage.index = null
+localStorage.editable = false
+const route = async (name = 'users', el) => {
+  // const pages = ['users', 'posts', 'comments']
+  const title = document.querySelector(`.box-title`)
+  const detail = document.querySelector(`#data-detail`)
+  const data = await getData(name)
+  switch (name) {
+    case 'users':
+      title.innerHTML = 'Users'
+      detail.innerHTML = ''
+      listUsers(data)
+      break;
+
+    case 'posts':
+      title.innerHTML = 'Posts'
+      detail.innerHTML = ''
+      listPost(data)
+      break;
+
+    case 'comments':
+      title.innerHTML = 'Comments'
+      detail.innerHTML = ''
+      listComment(data)
+      break;
+
+    default:
+      break;
   }
 }
+const getData = async (name) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/${name}`)
+  const json = await res.json()
+  localStorage.data = JSON.stringify(json)
+  return json
+}
+
 route()
